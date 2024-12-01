769c69d2d3137595bc90402fa1dbbb7363ab1b25
Andrew Marquez <andy2mgcc@gmail.com>
2024-12-01 15:51:57 -0700

    Solve day01, part 2

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -24,11 +24,31 @@ func partOne(left []int, right []int) int {
         return sum
 }
 
+func partTwo(left []int, right []int) int {
+        simScore := 0
+        cache := make(map[int]int) // number, occurrences
+        for _, num := range left {
+                occ, ok := cache[num]
+                if ok {
+                        simScore += num * occ
+                        continue
+                }
+                count := 0
+                for j := range right {
+                        if num == right[j] {
+                                count += 1
+                        }
+                }
+                simScore += num * count
+                cache[num] = count
+        }
+        return simScore
+}
+
 func main() {
         var left, right []int
         for idx, strNum := range strings.Fields(input) {
-                number, err := strconv.Atoi(strNum)
-                if err != nil {
+                number, err := strconv.Atoi(strNum); if err != nil {
                         panic(err)
                 }
                 if idx % 2 == 0 {
@@ -41,4 +61,5 @@ func main() {
         sort.Ints(right)
 
         fmt.Println("Part 1: ", partOne(left, right))
+        fmt.Println("Part 2: ", partTwo(left, right))
 }
