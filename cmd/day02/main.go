a5002c05247b2e796b6e7bfc0f15a3e866fca00e
Andrew Marquez <andy2mgcc@gmail.com>
2024-12-02 00:25:55 -0700

    Save day02 part 2 progress

--- cmd/day02/main.go
+++ cmd/day02/main.go
@@ -3,34 +3,22 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"strconv"
+	"internal/utils"
 	"strings"
 )
 
 //go:embed input.txt
 var input string
 
-func partOne(reports []string) int {
+func partOne(reports [][]int) int {
 	numSafe := 0
-	for _, report := range reports {
-		levels := strings.Fields(report)
-		prev, _ := strconv.Atoi(levels[0])
+	for _, levels := range reports {
+		prev := levels[0]
 		isSafe := false
-		dir := 0
-		for _, strLevel := range levels[1:] {
-			level, _ := strconv.Atoi(strLevel)
-			change := level - prev
-			if dir == 0 {
-				dir = change
-				if dir == 0 {
-					break
-				}
-			}
-			absChange := change
-			if change < 0 {
-				absChange *= -1
-			}
-			isSafe = change*dir > 0 && absChange >= 1 && absChange <= 3
+		_, currSign := utils.AbsInt(levels[1] - prev)
+		for _, level := range levels[1:] {
+			change, sign := utils.AbsInt(level - prev)
+			isSafe = sign == currSign && sign != 0 && change >= 1 && change <= 3
 			if !isSafe {
 				break
 			}
@@ -43,12 +31,65 @@ func partOne(reports []string) int {
 	return numSafe
 }
 
-func partTwo() int {
-	return 0
+func _getDomSign(ins []int) int {
+	sumSigns := 0
+	for i, item := range ins[1:] {
+		_, sign := utils.AbsInt(item - ins[i])
+		sumSigns += sign
+	}
+	_, domSign := utils.AbsInt(sumSigns)
+	return domSign
+}
+
+func peek(levels []int, i int) (int, bool) {
+	if i < len(levels) {
+		return levels[i], true
+	} else {
+		return 0, false
+	}
+}
+
+func isValidStep(prev int, curr int, domSign int) bool {
+	change, sign := utils.AbsInt(curr - prev)
+	return sign == domSign && change >= 1 && change <= 3
+}
+
+func partTwo(reports [][]int) int {
+	numSafe := 0
+	for _, levels := range reports {
+		prev := levels[0]
+		numUnsafe := 0
+		domSign := _getDomSign(levels)
+		for idx, curr := range levels[1:] {
+			isValid := isValidStep(prev, curr, domSign)
+			if isValid {
+				prev = curr
+				continue
+			}
+			numUnsafe += 1
+			next, ok := peek(levels, idx+2)
+			if ok {
+				nextValid := isValidStep(curr, next, domSign)
+				if nextValid {
+					prev = curr
+				}
+			}
+		}
+		if numUnsafe <= 1 {
+			numSafe += 1
+		}
+	}
+	return numSafe
+}
+
+func processInput(i string) [][]int {
+	return utils.Map(strings.Split(i, "\n"), func(report string) []int {
+		return utils.Map(strings.Fields(report), utils.StrToInt)
+	})
 }
 
 func main() {
-	reports := strings.Split(input, "\n")
+	reports := processInput(input)
 	fmt.Println("Part 1: ", partOne(reports))
-	fmt.Println("Part 2: ", partTwo())
+	fmt.Println("Part 2: ", partTwo(reports))
 }
