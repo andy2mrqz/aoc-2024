abd836d946125d75a0003eef777b78102d7a1889
Andrew Marquez <andy2mgcc@gmail.com>
2024-12-05 01:41:38 -0700

    Solve day 04 part 2

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -17,21 +17,8 @@ func (g Grid) Next(p Point, dir Point) (Point, string) {
 	nextChar := g[nextPoint]
 	return nextPoint, nextChar
 }
-func partOne(i string) int {
-	// create a grid of cells
-	grid := Grid{}
-	// find all instances of cell 'X'
-	xPoints := []Point{}
-	for y, line := range strings.Split(i, "\n") {
-		for x, char := range line {
-			point := Point{x, y}
-			if char == 'X' {
-				xPoints = append(xPoints, point)
-			}
-			grid[point] = string(char)
-		}
-	}
-	// perform the search
+
+func partOne(grid Grid, xPoints []Point) int {
 	sum := 0
 	dirs := []Point{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}
 	for _, xPoint := range xPoints {
@@ -50,7 +37,43 @@ func partOne(i string) int {
 	return sum
 }
 
+func partTwo(grid Grid, aPoints []Point) int {
+	sum := 0
+	for _, aPoint := range aPoints {
+		_, nw := grid.Next(aPoint, Point{-1, -1})
+		_, ne := grid.Next(aPoint, Point{1, -1})
+		_, sw := grid.Next(aPoint, Point{-1, 1})
+		_, se := grid.Next(aPoint, Point{1, 1})
+		leg1, leg2 := nw+se, ne+sw
+		if (leg1 == "MS" || leg1 == "SM") && (leg2 == "MS" || leg2 == "SM") {
+			sum += 1
+		}
+	}
+	return sum
+}
+
+func initGrid(i string, charStart1 rune, charStart2 rune) (Grid, []Point, []Point) {
+	// create a grid of cells
+	grid := Grid{}
+	// find all instances of runes of interest
+	charPoints1 := []Point{}
+	charPoints2 := []Point{}
+	for y, line := range strings.Split(i, "\n") {
+		for x, char := range line {
+			point := Point{x, y}
+			if char == charStart1 {
+				charPoints1 = append(charPoints1, point)
+			} else if char == charStart2 {
+				charPoints2 = append(charPoints2, point)
+			}
+			grid[point] = string(char)
+		}
+	}
+	return grid, charPoints1, charPoints2
+}
+
 func main() {
-	fmt.Println("Part 1: ", partOne(input))
-	// fmt.Println("Part 2: ", 0)
+	grid, charPoints1, charPoints2 := initGrid(input, 'X', 'A')
+	fmt.Println("Part 1: ", partOne(grid, charPoints1))
+	fmt.Println("Part 2: ", partTwo(grid, charPoints2))
 }
